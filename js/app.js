be20b9ec5fd2f14e24d2b160127e26256851dc14
Valentina Iramain <valentinairamain2020@gmail.com>
2025-05-15 18:42:06 -0300

    Se realiza el reloj segun lo visto en clase

---
+++ js/app.js
@@ -0,0 +1,94 @@
+
+function obtenerFechayHora() {
+    // objeto que tendra fecha y hora
+    const fecha = new Date()
+
+    // obtiene el dia de la semana en texto
+    const diaSemana = fecha.getDay()
+    console.log('diasemana:' + diaSemana)
+    let diaLetras = ''
+    switch (diaSemana) {
+        case 0:
+            diaLetras = 'Domingo'
+            break;
+        case 1:
+            diaLetras = 'Lunes'
+            break;
+        case 2:
+            diaLetras = 'Martes'
+            break;
+        case 3:
+            diaLetras = 'Miércoles'
+            break;
+        case 4:
+            diaLetras = 'Jueves'
+            break;
+        case 5:
+            diaLetras = 'Viernes'
+            break;
+        case 6:
+            diaLetras = 'Sábado'
+            break;
+        default:
+            break;
+    }
+
+    // obtiene el mes
+    const mes = fecha.getMonth()
+    console.log('mes:' + mes)
+    let mesLetras = ''
+    switch (mes) {
+        case 0:
+            mesLetras = 'Enero'
+            break;
+        case 1:
+            mesLetras = 'Febrero'
+            break;
+        case 2:
+            mesLetras = 'Marzo'
+            break;
+        case 3:
+            mesLetras = 'Abril'
+            break;
+        case 4:
+            mesLetras = 'Mayo'
+            break;
+        case 5:
+            mesLetras = 'Junio'
+            break;
+        case 6:
+            mesLetras = 'Julio'
+            break;
+        case 7:
+            mesLetras = 'Agosto'
+            break;
+        case 8:
+            mesLetras = 'Septiembre'
+            break;
+        case 9:
+            mesLetras = 'Octubre'
+            break;
+        case 10:
+            mesLetras = 'Noviembre'
+            break;
+        case 11:
+            mesLetras = 'Diciembre'
+            break;
+
+        default:
+            break;
+    }
+
+    const textoFecha = document.querySelector('#textoFecha')
+    const hora = document.querySelector('#hora')
+    const segundos = fecha.getSeconds() < 10 ? '0' + fecha.getSeconds() : fecha.getSeconds();
+    const minutos = fecha.getMinutes() < 10 ? '0' + fecha.getMinutes() : fecha.getMinutes();
+    const horas = fecha.getHours() < 10 ? '0' + fecha.getHours() : fecha.getHours();
+
+    hora.textContent = `${horas}:${minutos}:${segundos}`
+    textoFecha.textContent = `${diaLetras} ${fecha.getDate()} de ${mesLetras} de ${fecha.getFullYear()}`
+}
+
+
+setInterval(obtenerFechayHora, 1000)
+
